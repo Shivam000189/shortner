75a3680b9b22ca8a91bae1ec2880c27f7147d6d7
Shivam000189 <shivamsharmass9897@gmail.com>
2025-10-28 23:52:31 +0530

    dataBase

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://localhost:27017//urlShortener")
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.error('❌ MongoDB connection error:', err));
+
+module.exports = mongoose
+
+
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+require('./config/db');
+
 const app = express();
 const PORT = 3001; 
 
 
 app.get('/', (req, res) => {
-    res.send();
+    res.send("welcome to URL Shortner API");
 });
 
-
 app.listen(PORT, () => {
-    console.log("Server is running");
+    console.log(`Server is running on port ${PORT}`);
 });
