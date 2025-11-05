f3d0e48adbd02a5ea94fa6e2f1305a97bc0beb6a
Shivam000189 <shivamsharmass9897@gmail.com>
2025-11-05 23:41:35 +0530

    ROutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,3 +1,15 @@
 const express = require('express');
 
-const 
+const brcypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.post("/register", async(req, res) => {
+    
+})
