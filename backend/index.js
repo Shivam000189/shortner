6ef10afd72d07b24c977686aae56e9896ed0b555
Shivam000189 <shivamsharmass9897@gmail.com>
2025-11-15 22:31:10 +0530

    Redirect URL

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use('/', authRoutes);
 
 const urlRoutes = require('./routes/urlRoutes');
+const Url = require('./models/Url');
 app.use('/', urlRoutes);
 
 
@@ -25,6 +26,25 @@ app.use('/', urlRoutes);
 app.get('/', (req, res) => {
     res.send("welcome to URL Shortner API");
 });
+
+app.get(':/shortCode', async (req, res) => {
+    try{
+       const {shortCode} = req.params;
+       
+       const url = await Url.findOne({shortCode});
+
+       if(!url) {
+        return res.status(404).json({msg:"Short URL not found"});
+       }
+       url.clicks += 1;
+       await url.save();
+
+       return res.redirect(url.originalUrl);
+    } catch(error){
+        console.error('Redirect Error:', error);
+        return res.status(500).json({ message: 'Server Error' });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
